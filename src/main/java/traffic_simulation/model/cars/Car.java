24ec2f686d4b91d7011b588c8967775250a94b47
janekVonLipinski <janekvonlipinski222@gmail.com>
2025-12-05 11:13:28 +0100

    started implementing car logic

--- src/main/java/traffic_simulation/model/cars/Car.java
+++ src/main/java/traffic_simulation/model/cars/Car.java
@@ -1,13 +1,24 @@
 package traffic_simulation.model.cars;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import traffic_simulation.model.Point;
 import traffic_simulation.model.street_network.Street;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Car {
 
     private final Street currentStreet;
     private final double velocity;
+    private Point location;
 
 
+    public void drive() {
+
+        Point direction = currentStreet.getDirection();
+        double streetLength = currentStreet.getLength();
+
+
+
+    }
 }
--- src/main/java/traffic_simulation/model/street_network/street_network_points/SpawnPoint.java
+++ src/main/java/traffic_simulation/model/street_network/street_network_points/SpawnPoint.java
@@ -1,6 +1,8 @@
 package traffic_simulation.model.street_network.street_network_points;
 
-import traffic_simulation.cars.Car;
+
+import traffic_simulation.model.Point;
+import traffic_simulation.model.cars.Car;
 import traffic_simulation.model.street_network.GridPoint;
 import traffic_simulation.model.street_network.Street;
 
@@ -12,10 +14,14 @@ public class SpawnPoint extends GridPoint {
     private final double spawnTick;
     private final Random random = new Random();
     private final Street street;
+    private static final int EXPECTED_VALUE = 45;
+    private static final int DEVIATION = 10;
+    private static final double CONVERSION_FACTOR_KM_PER_H_T_M_PER_S = 3.6;
     private int currentTick = 0;
 
-    public SpawnPoint(double x, double y, double spawnTick, Street street) {
-        super(x, y);
+
+    public SpawnPoint(Point point, double spawnTick, Street street) {
+        super(point);
         this.spawnTick = spawnTick;
         this.street = street;
     }
@@ -29,7 +35,10 @@ public class SpawnPoint extends GridPoint {
             return null;
         }
 
-        return new Car(street);
+        double carVelocityInKmPerH = random.nextGaussian(EXPECTED_VALUE, DEVIATION);
+        double carVelocityInMPerS = carVelocityInKmPerH / CONVERSION_FACTOR_KM_PER_H_T_M_PER_S;
+
+        return new Car(street, carVelocityInMPerS, getPoint()) ;
     }
 
 }

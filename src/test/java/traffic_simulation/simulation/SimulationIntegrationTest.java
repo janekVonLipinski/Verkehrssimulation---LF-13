39893c48a6c9e01fc2ddb049f702a2d7fee44762
Alex <zandi1805@gmail.com>
2026-02-27 11:24:28 +0100

    changed car model

---
+++ src/test/java/traffic_simulation/simulation/SimulationIntegrationTest.java
@@ -0,0 +1,40 @@
+package traffic_simulation.simulation;
+
+import org.junit.jupiter.api.Test;
+import traffic_simulation.model.Point;
+import traffic_simulation.model.cars.Car;
+import traffic_simulation.model.street_network.Street;
+import traffic_simulation.model.street_network.street_network_points.SpawnPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationIntegrationTest {
+
+
+    @Test
+    void given_two_node_city_then_one_car_reaches_destination() {
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+
+
+        SpawnPoint sp1 = new SpawnPoint(p1, 3, null,null);
+        SpawnPoint sp2 = new SpawnPoint(p2, 3, null,null);
+
+        Street street = new Street(sp1, sp2, 1);
+        sp1.setStreet(street);
+        sp2.setStreet(street);
+
+        Simulation simulation = new Simulation(
+                List.of(sp1, sp2)
+        );
+
+        List<Car> remainingCars = simulation.simulate(2);
+
+        assertEquals(0, remainingCars.size());
+    }
+
+}
